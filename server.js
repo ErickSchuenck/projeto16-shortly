be3e5cdd119dd82b5e43807feca34b1a589a014e
ErickSchuenck <esfmokdeci@gmail.com>
2022-06-08 15:14:22 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Servidor ligado na porta ${port}`)
+});
