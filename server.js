0a29715460268dc7a6cdb49560dcfa39c3874d46
ErickSchuenck <esfmokdeci@gmail.com>
2022-06-08 15:40:46 -0300

    chalk instalado

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import chalk from 'chalk';
 
 dotenv.config();
 
@@ -10,5 +11,10 @@ app.use(express.json());
 
 let port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Servidor ligado na porta ${port}`)
+  console.log(chalk.blue(`
+  ________________________________
+  
+  Servidor ligado na porta ${port}
+  ________________________________
+  `))
 });
